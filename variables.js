26926c0933dffe5c19cd80cd2c957e779ce9296a
Love <lovesoni607@gmail.com>
2023-10-02 00:00:40 +0530

    let variable

--- variables.js
+++ variables.js
@@ -8,3 +8,26 @@ function test() {
 var x = 50;
 console.log(x);
 // value of x will be 50 now
+
+var blank;
+console.log(blank); // blan
+
+
+// Issue with variable
+var flag = "hi love";
+var y = 2;
+if (y > 0) {
+    var flag = 'hi you are hacked';
+}
+console.log(flag);
+
+
+// let
+// scope: block scope
+
+let myFlag = "hi love";
+let y1 = 2;
+if (y1 > 0) {
+    let myFlag = 'hi you are hacked';
+}
+console.log(myFlag);
