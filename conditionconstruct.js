e3986462bfcb2ee10ab1e6c44f49619bcc35214b
Love <lovesoni607@gmail.com>
2023-10-02 00:05:13 +0530

    if else

---
+++ conditionconstruct.js
@@ -0,0 +1,9 @@
+function checkAge(age) {
+    if (age >= 18) {
+        console.log('you are eligible to vote');
+    } else {
+        console.log('you are not eligible to vote');
+    }
+}
+
+checkAge(17);
